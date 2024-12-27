e943641b4731c94b2aa58f2d8bf2e2d259282344
Oscar Morris <twocap06@gmail.com>
2024-12-27 20:09:22 +0000

    processed QT 25-27

--- split_audio.py
+++ split_audio.py
@@ -8,7 +8,7 @@ import pickle
 from datastructs import Node, Relation, Segment
 import alignment
 
-QT_EPISODE = "24.19August2021"
+QT_EPISODE = "27.30September2021"
 
 ALIGNMENTS_PATH = f"data/Question Time/{QT_EPISODE}/alignments.json"
 ARGUMENT_MAP_PATH = f"data/Question Time/{QT_EPISODE}/argument_map.json"
